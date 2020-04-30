b1bd72237609ed792a72c09b8d252d7579a80d7a
Milan <milan_susa@hotmail.com>
2020-04-30 19:06:58 +0200

    Add Logo component

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../../assets/images/molecious-logo.png";
+
+const Logo = props => {
+    return (
+        <div className="text-center">
+            <img src={logo} alt="" />
+        </div>
+    );
+};
+
+export default Logo;
