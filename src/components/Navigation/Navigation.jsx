9e052458b3ab09b1c7a6cee512119517b1ed6cc1
Milan <milan_susa@hotmail.com>
2020-05-01 16:46:17 +0200

    Add React Bootstrap and Navigation component

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+const Navigation = props => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+            <Navbar.Brand href="/home">Home</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="/signup">Sign up</Nav.Link>
+                    <Nav.Link href="/login">Login</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
+
+export default Navigation;
